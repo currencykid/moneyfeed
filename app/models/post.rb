e6c0b8f241c65ee2f978aa2416989f435f4de6d0
ck <thecurrencykid@gmail.com>
2016-05-03 15:34:47 -0700

    before comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
-  default_scope { order('created_at DESC') }
+  default_scope { order('cached_votes_score DESC') }
 
   validates :title, presence: true, length: {minimum: 1, maximum: 300}
   validates :url, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503203002) do
+ActiveRecord::Schema.define(version: 20160503215709) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "user_id"
+    t.integer  "cached_votes_total", default: 0
+    t.integer  "cached_votes_score", default: 0
+    t.integer  "cached_votes_up",    default: 0
+    t.integer  "cached_votes_down",  default: 0
   end
 
+  add_index "posts", ["cached_votes_down"], name: "index_posts_on_cached_votes_down"
+  add_index "posts", ["cached_votes_score"], name: "index_posts_on_cached_votes_score"
+  add_index "posts", ["cached_votes_total"], name: "index_posts_on_cached_votes_total"
+  add_index "posts", ["cached_votes_up"], name: "index_posts_on_cached_votes_up"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
