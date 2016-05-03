56c65b42954b0c8051f6df8a97aee288229bc2d6
ck <thecurrencykid@gmail.com>
2016-05-03 16:51:58 -0700

    before uniq username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,21 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+
+  before_save { self.email = email.downcase }
+  before_save { self.username = username.downcase }
   has_many :posts
+
+  validates :username, :presence => true, :uniqueness => true
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
