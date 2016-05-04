d8d4ecb24b9e95ab65c95f58f7f61b1a994c6aae
ck <thecurrencykid@gmail.com>
2016-05-03 17:17:55 -0700

    lala

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
+  before_save { |user| user.username = user.username.downcase }
   before_save { self.email = email.downcase }
-  before_save { self.username = username.downcase }
   has_many :posts
 
   validates :username, :presence => true, :uniqueness => true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503215709) do
+ActiveRecord::Schema.define(version: 20160504000117) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
