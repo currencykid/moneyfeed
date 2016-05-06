952898e95d76de3015f009682c9d5f40073f959b
currencykid <thecurrencykid@gmail.com>
2016-05-05 17:57:33 -0700

    dropped comments table

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :commentable, polymorphic: true
-
-
-  validates :body, presence: true, length: {minimum: 2, maximum: 500}
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505194549) do
+ActiveRecord::Schema.define(version: 20160506005541) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commentable_type"
