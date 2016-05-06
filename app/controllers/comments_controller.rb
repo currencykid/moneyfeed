3da877bce12dc3d7e1da7e7246197f881a4a47f5
currencykid <thecurrencykid@gmail.com>
2016-05-05 21:14:52 -0700

    lala

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,7 +27,7 @@ class CommentsController < ApplicationController
 
   def update
     if @comment.update(params[:comment].permit(:content)) 
-    redirect_to :back
+    redirect_to post_path(@post)
     else
       render 'edit' 
     end
