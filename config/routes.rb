61748472363d288ea4c545fe1c9de5fa8b910fbc
ck <thecurrencykid@gmail.com>
2016-05-09 15:26:16 -0700

    user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   resources :posts do 
         member do 
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
   end 
 
   resources :comments
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
