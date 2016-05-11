8f64172c47e34202e2e5be8513b10e9aad264e1e
ck <thecurrencykid@gmail.com>
2016-05-10 23:20:29 -0700

    removed merit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511050307) do
+ActiveRecord::Schema.define(version: 20160511050236) do
 
   create_table "badges_sashes", force: :cascade do |t|
     t.integer  "badge_id"
@@ -105,8 +105,6 @@ ActiveRecord::Schema.define(version: 20160511050307) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.integer  "sash_id"
-    t.integer  "level",                  default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
