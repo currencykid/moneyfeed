9ed1653a2bd3c249e189334d0d96c9e9cb455068
ck <thecurrencykid@gmail.com>
2016-05-13 23:24:42 -0700

    dependent destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, dependent: :destroy
+  belongs_to :user, dependent: :destroy 
   validates :content, presence: true, length: {minimum: 2, maximum: 500}
+
 
 end
