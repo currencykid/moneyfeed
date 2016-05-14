1468a00f966225266d3fbbf970254c1dc26ff8f8
ck <thecurrencykid@gmail.com>
2016-05-14 13:49:19 -0700

    removed devise mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   extend FriendlyId
     friendly_id :username, use: [:slugged]
 
-  devise :database_authenticatable, :registerable, :confirmable, 
+  devise :database_authenticatable, :registerable,  
     :recoverable, :rememberable, :trackable, :validatable
 
   before_save { |user| user.username = user.username.downcase }
