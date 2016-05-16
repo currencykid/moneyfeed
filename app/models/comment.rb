3500ada44f6053b8328a303d1313cbab6837454c
ck <thecurrencykid@gmail.com>
2016-05-15 20:12:03 -0700

    wtf

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  belongs_to :user , dependent: :destroy
+  belongs_to :user, dependent: :destroy
   validates :content, presence: true, length: {minimum: 2, maximum: 500}
 
 
